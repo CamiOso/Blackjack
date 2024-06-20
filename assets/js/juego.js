bedceb4b92609a558f088e8f7100575ff797c7ed
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-06-20 06:11:33 -0500

    Se crea la funcion para saber el valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,65 +1,54 @@
-
 /**
  * 2C = Two of Clubs (Tréboles)
- * 2D = Two of Diamonds  
- * 2H = Two of Hearts  
- * 2S = Two of Spades  
+ * 2D = Two of Diamonds
+ * 2H = Two of Hearts
+ * 2S = Two of Spades
  */
 
 let deck = [];
-const tipos = ['C','D','H','S'];
-const especiales = ['A','J','Q','K'];
+const tipos = ["C", "D", "H", "S"];
+const especiales = ["A", "J", "Q", "K"];
 
 // Esta función crea un nuevo deck
-const crearDeck=()=>{
-    for(let i=2;i<10;i++){
-       for(let tipo of tipos){
-              deck.push(i+tipo);
-
-       }
-
+const crearDeck = () => {
+  for (let i = 2; i < 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
     }
+  }
 
-    for(let tipo of tipos){
-
-        for(let esp of especiales){
-            deck.push(esp+tipo);
-        }
+  for (let tipo of tipos) {
+    for (let esp of especiales) {
+      deck.push(esp + tipo);
     }
+  }
 
+  deck = _.shuffle(deck);
+  console.log(deck);
+  return deck;
+};
 
-
-   
-    deck = _.shuffle(deck);
-    console.log(deck);
-    return deck;
-
- 
-
-}
-
-
-crearDeck();
+deck=crearDeck();
 
 // Esta función me permite tomar una carta
 
-const pedirCarta=()=>{
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
 
-    if(deck.length===0){
-        throw 'No hay cartas en el deck';
-
-    }
-
-    const carta=deck.pop();
-    console.log(deck);
-    console.log(carta);
-return carta;
+  const carta = deck.pop();
+  console.log(deck);
+  console.log(carta);
+  return carta;
+};
 
  
 
-}
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
 
-
-
-
-pedirCarta();
+const valor=valorCarta(pedirCarta());
+console.log({valor});
