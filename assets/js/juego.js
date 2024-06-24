beb005ec33a06d344a467643c1054cae62e5810f
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-06-24 06:35:35 -0500

    Se crea el evento click de Pedir Carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,13 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
+let puntosJugador = 0,
+  puntosComputadora = 0;
+
+//Referencias del HTML
+const btnPedir = document.querySelector("#btnPedir");
+const puntosHTML=document.querySelectorAll('small');
+
 // Esta función crea un nuevo deck
 const crearDeck = () => {
   for (let i = 2; i < 10; i++) {
@@ -28,7 +35,7 @@ const crearDeck = () => {
   return deck;
 };
 
-deck=crearDeck();
+deck = crearDeck();
 
 // Esta función me permite tomar una carta
 
@@ -38,17 +45,25 @@ const pedirCarta = () => {
   }
 
   const carta = deck.pop();
-  console.log(deck);
-  console.log(carta);
+
   return carta;
 };
-
- 
 
 const valorCarta = (carta) => {
   const valor = carta.substring(0, carta.length - 1);
   return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
 };
 
-const valor=valorCarta(pedirCarta());
-console.log({valor});
+const valor = valorCarta(pedirCarta());
+
+//Eventos
+
+btnPedir.addEventListener("click", () => {
+  const carta = pedirCarta();
+  puntosJugador=puntosJugador+valorCarta(carta);
+puntosHTML[0].innerText=puntosJugador;
+  
+ 
+
+ 
+});
